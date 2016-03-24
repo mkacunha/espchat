296e50cae380c4617d7ec3725e4e44e16d750d3f
Maiko Cunha <mkacunha@gmail.com>
2016-03-23 21:46:31 -0300

    Criado um filter para mais tarde fazer a validação de usuário logado

---
+++ src/main/java/br/mkacunha/espchat/filter/FiltroUsuarioLogado.java
@@ -0,0 +1,39 @@
+package br.mkacunha.espchat.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+
+@WebFilter(urlPatterns = "/*")
+public class FiltroUsuarioLogado implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+
+		System.out.println("===> ULR: " + request.getRequestURI());
+
+		chain.doFilter(req, resp);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
